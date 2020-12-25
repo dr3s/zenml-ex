a9f0b5b92a0962c0edd083925c44ad0bf8c7030f
dr3s <andres.march@gmail.com>
2020-12-25 16:54:45 -0500

    test

---
+++ test.py
@@ -0,0 +1,45 @@
+from zenml.core.datasources.csv_datasource import CSVDatasource
+from zenml.core.pipelines.training_pipeline import TrainingPipeline
+from zenml.core.steps.evaluator.tfma_evaluator import TFMAEvaluator
+from zenml.core.steps.preprocesser.standard_preprocesser.standard_preprocesser import StandardPreprocesser
+from zenml.core.steps.split.random_split import RandomSplit
+from zenml.core.steps.trainer.feedforward_trainer import FeedForwardTrainer
+
+training_pipeline = TrainingPipeline(name='Quickstart')
+
+# Add a datasource. This will automatically track and version it.
+ds = CSVDatasource(name='Pima Indians Diabetes Dataset', 
+                   path='gs://zenml_quickstart/diabetes.csv')
+training_pipeline.add_datasource(ds)
+
+# Add a random 70/30 train-eval split
+training_pipeline.add_split(RandomSplit(split_map={'train': 0.7, 'eval': 0.3}))
+
+# StandardPreprocesser() has sane defaults for normal preprocessing methods
+training_pipeline.add_preprocesser(
+    StandardPreprocesser(
+        features=['times_pregnant', 'pgc', 'dbp', 'tst', 'insulin', 'bmi',
+                  'pedigree', 'age'],
+        labels=['has_diabetes'],
+        overwrite={'has_diabetes': {
+            'transform': [{'method': 'no_transform', 'parameters': {}}]}}
+    ))
+
+# Add a trainer
+training_pipeline.add_trainer(FeedForwardTrainer(
+    loss='binary_crossentropy',
+    last_activation='sigmoid',
+    output_units=1,
+    metrics=['accuracy'],
+    epochs=20))
+
+
+# Add an evaluator
+training_pipeline.add_evaluator(
+    TFMAEvaluator(slices=[['has_diabetes']],
+                  metrics={'has_diabetes': ['binary_crossentropy',
+                                            'binary_accuracy']}))
+
+# Run the pipeline locally
+training_pipeline.run()
+
